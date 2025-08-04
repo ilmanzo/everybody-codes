1ed4b93b73010268bb33418e81d213944e66b10c
Andrea Manzini <andrea@localhost.localdomain>
2025-08-04 12:24:21 +0200

    q8 + q9/1

---
+++ 2024/09/09a.rb
@@ -0,0 +1,25 @@
+stamps=[1, 3, 5, 10].freeze
+
+
+def min_stamps(stamps, amount)
+  dp = Array.new(amount + 1, amount + 1)
+  dp[0] = 0
+  (1..amount).each do |a|
+    stamps.each do |stamp|
+      if stamp <= a
+        dp[a] = [dp[a], 1 + dp[a - stamp]].min
+      end
+    end
+  end
+  dp[amount] > amount ? -1 : dp[amount]
+end
+
+
+FILENAME=ARGV.length>0 ? ARGV[0] : 'samplea.txt'
+input = File.open(FILENAME, 'r').readlines.map(&:to_i)
+
+result= input.map do |i|
+  min_stamps(stamps, i)
+end.sum
+
+p result
