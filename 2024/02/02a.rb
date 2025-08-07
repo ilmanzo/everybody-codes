abb9fb2290961eecf6b54f7d5bfa16334f2aad9e
Andrea Manzini <andrea@localhost.localdomain>
2025-08-07 22:51:46 +0200

    event2024, 12-13 story1 1/2/3

--- 2024/02/02a.rb
+++
@@ -1,8 +0,0 @@
-FILENAME="input1.txt"
-#FILENAME="sample.txt"
-input_lines = File.read(FILENAME).lines
-words = input_lines[0].split(':')[1].strip.split(',')
-
-sentence=input_lines[2].strip
-# count how many times each word appears in the sentence
-p words.map {|w| sentence.scan(w).count}.sum
